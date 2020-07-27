c89867c3d9fcdd38616de788ad3bf27b540fc35e
samuel-p <codes@samuel-philipp.de>
2020-07-27 17:41:08 +0200

    restructured project (closes #5)

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,20 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addWatchTarget("./scss/");
+    eleventyConfig.addWatchTarget("./src/scss/");
 
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
     eleventyConfig.setUseGitIgnore(false);
     eleventyConfig.addPassthroughCopy({
-        "img": "img",
-        "font": "font",
+        "src/img": "img",
+        "src/font": "font",
         "node_modules/@fortawesome/fontawesome-free/webfonts/": "font",
         "node_modules/flag-icon-css/flags/4x3/(de|us)*": "flags"
     });
 
     return {
         dir: {
-            input: "sites",
+            input: "src",
             includes: "_includes",
             layouts: "_includes/layouts",
             output: "dist"
